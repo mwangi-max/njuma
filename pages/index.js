5c56b96dd31bc67d1a0fc5611e934d1fa7a13b58
Kelvin Kamau <80668152+mwangi-max@users.noreply.github.com>
2023-03-11 17:07:09 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <h2>KELVIN KAMAU<h2/>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
